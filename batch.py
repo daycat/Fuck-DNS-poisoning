b9f25b4ce561ac513f1aae0745a7f1f07051f399
daycat <iyasmalan@gmail.com>
2022-03-16 18:03:45 +0800

    Optimized Ping Process, and added new mode to get multiple usable IPs

--- batch.py
+++ batch.py
@@ -1,7 +1,11 @@
+"""
+This is the batch version for FDP to get a range of IPs that are usable from a domain
+"""
 import requests
 import ping
 import json
 domain = input("Domain:")
+search_times = int(input("Depth:"))
 headers = {
     'accept': 'application/dns-json',
 }
@@ -12,7 +16,8 @@ params = (
 )
 got_usable_ip = False
 IPs = []
-while not got_usable_ip:
+Usable_IPs = []
+for x in range (search_times):
     try:
         response = requests.get('https://fdp.n100.workers.dev/dns-query', headers=headers, params=params)
     except:
@@ -21,14 +26,21 @@ while not got_usable_ip:
     data = {}
     data = json.loads(response.text)
     ip = data['Answer'][0]['data']
-    if ip in IPs:
-        pass
-    else:
+    if ip not in IPs:
         print("new IP " + ip + " from fdp")
         if ping.hostping(ip):
             got_usable_ip = True
-            print("Found Usable IP: "+ip)
+            IPs.append(ip)
+            print("Found Usable IP: " + ip)
+            if ip not in Usable_IPs:
+                Usable_IPs.append(ip)
         else:
-            print("IP "+ ip +" not reachable by ICMP")
+            print("IP " + ip + " not reachable by ICMP")
             IPs.append(ip)
             pass
+    else:
+        pass
+c=0
+for y in Usable_IPs:
+    print(Usable_IPs[c])
+    c+=1
--- ping.py
+++ ping.py
@@ -3,5 +3,5 @@ import subprocess
 
 def hostping(host):
     param = '-n' if platform.system().lower()=='windows' else '-c'
-    command = ['ping', param, '1', host]
+    command = ['ping', param, '1', '-W 1', host]
     return subprocess.call(command) == 0
